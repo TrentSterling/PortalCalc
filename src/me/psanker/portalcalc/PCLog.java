4eea07da90450ce048302f22fc32fc87c199644a
psanker <patricksanker@gmail.com>
2011-07-09 00:00:24 -0600

    Cleaned PCLog

--- src/me/psanker/portalcalc/PCLog.java
+++ src/me/psanker/portalcalc/PCLog.java
@@ -8,16 +8,11 @@ public class PCLog {
     private static final Logger logger = Logger.getLogger("Minecraft");
     
     public static void log(String string, int level) {
-        if (level == 0) {
-            logger.log(Level.INFO, "[PortalCalc] " + string, "[PortalCalc]");
-        }
-        
-        if (level == 1) {
-            logger.log(Level.WARNING, "[PortalCalc] " + string, "[PortalCalc]");
-        }
-        
-        if (level == 2) {
-            logger.log(Level.SEVERE, "[PortalCalc] " + string, "[PortalCalc]");
-        }
+        if (level == 0)
+            logger.log(Level.INFO, "[PortalCalc] " + string);
+        if (level == 1)
+            logger.log(Level.WARNING, "[PortalCalc] " + string);
+        if (level == 2)
+            logger.log(Level.SEVERE, "[PortalCalc] " + string);
     }
 }
